e52748073d2d539aa1940369169f9d08853b962e
MinhoPotato <Noelkids2409@gmail.com>
2025-02-02 13:40:02 -0500

    finetuning

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,23 +4,30 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    numItems: 0
   },
+
   reducers: {
     addItem: (state, action) => {
         const { name, image, cost } = action.payload;
         const existingItem = state.items.find(item => item.name === name);
         if (existingItem)  {
             existingItem.quantity++;
-        }else{
+        }
+        else{
             state.items.push({ name, image, cost, quantity: 1});
         }
+        state.numItems += 1;
     },
     removeItem: (state, action) => {
-        state.items = state.itemes.filter(item => item.name !== action.payload);
+        const { name, quantity } = action.payload;
+        state.items = state.items.filter(item => item.name !== name);
+        state.numItems -= item.quantity;
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
         const existingItem = state.items.find(item => item.name === name);
+        
         if (existingItem){
             existingItem.quantity = existItem.quantity + quanitity;
         }
