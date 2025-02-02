44000045ccff69698f9fd6c927d07554aecd9ddf
MinhoPotato <Noelkids2409@gmail.com>
2025-02-02 13:36:04 -0500

    finetuning

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,14 +7,6 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
-    const handleAddToCart = (product) => {
-        dispatch(addItem(product));
-        setAddedToCart((prevState) => ({
-            ...prevState,
-            [product.name]: true,
-        }));
-    };
-
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -256,6 +248,14 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+  const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+        ...prevState,
+        [product.name]: true,
+    }));
+};
     return (
         <div>
              <div className="navbar" style={styleObj}>
