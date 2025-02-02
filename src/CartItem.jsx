4ed316c31d0875a9ae0163c9ebde836c234b3515
MinhoPotato <Noelkids2409@gmail.com>
2025-02-02 13:54:13 -0500

    fixing total cost fucntion

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -10,7 +10,7 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
     return cart.reduce((total, item) => {
-        return total + item.cost * item.quantity;
+        return total + item.quantity * parseFloat(item.cost.replace('$', ''));
     }, 0);
   };
 
